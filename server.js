91de3c06db0eba28023cdedd5394a231ad6aeff4
InezHernandez <hernanin@oregonstate.edu>
2016-11-17 17:39:54 -0800

    Inez Here - Uploading some files
    Here are some files that I have written at this point, not the ones we will be using in most probability since
    Tiara might have some more edited versions.

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+//var staticDir = path.join(__dirname, 'public');
+var indexFilename = 'homepage.html';
+// var notFoundFilename = '404.html';
+var port = process.env.PORT || 3000;
+
+
+var contents = fs.readFileSync('homepage.html', 'utf8');
+var style = fs.readFileSync('style.css', 'utf8');
+// var four = fs.readFileSync(staticDir + '/404.html', 'utf8');
+// var index = fs.readFileSync(staticDir + '/index.js', 'utf8');
+const server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+if (req.url == "homepage.html" || req.url == "/") {
+    res.statusCode = 200;
+    res.write(contents);
+    res.end();
+}
+else if (req.url == "style.css") {
+    res.write(style);
+    res.statusCode = 200;
+    res.end();
+}
+
+// else if (req.url == "/index.js") {
+//     res.write(index);
+//     res.statusCode = 200;
+//     res.end();
+// }
+//
+// else {
+//     res.write(four);
+//     res.statusCode = 404;
+//     res.end();
+// }
+
+}
+server.listen(port);
+console.log(" == Listening on port", port);
